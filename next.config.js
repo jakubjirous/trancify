7613501eaf4e6408e03ea80637dad0cbd4ac9d9f
Jakub Jirous <info@jakubjirous.dev>
2024-10-04 11:09:11 -0700

    core: update next config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  devIndicators: {
+    appIsrStatus: false,
+  },
+  experimental: {
+    ppr: true,
+    dynamicIO: true,
+    reactCompiler: true,
+    serverActions: {
+      bodySizeLimit: "5mb",
+    },
+  },
+};
 
 module.exports = nextConfig;
