3cbb671f8eea4a7231e53109fc815df2bcc0c242
Jakub Jirous <info@jakubjirous.dev>
2024-10-10 16:11:18 -0700

    core: update providers

--- src/providers/global-providers.tsx
+++ src/providers/global-providers.tsx
@@ -1,7 +1,7 @@
 import { ThemeProvider } from "next-themes";
 import { ReactNode } from "react";
 
-export default function Providers({ children }: { children: ReactNode }) {
+export default function GlobalProviders({ children }: { children: ReactNode }) {
   return (
     <ThemeProvider
       attribute="class"
---
+++ src/providers/protected-providers.tsx
@@ -0,0 +1,28 @@
+import ROUTES from "@/config/routes";
+import { PlaylistProvider } from "@/hooks/use-playlist";
+import { getAllPlaylists } from "@/lib/db/queries";
+import { createClient } from "@/lib/supabase/server";
+import { redirect } from "next/navigation";
+import { ReactNode } from "react";
+
+export default async function ProtectedProviders({
+  children,
+}: { children: ReactNode }) {
+  const supabase = createClient();
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  if (!user) {
+    return redirect(ROUTES.signIn);
+  }
+
+  const playlistsPromise = getAllPlaylists(user);
+
+  return (
+    <PlaylistProvider playlistsPromise={playlistsPromise}>
+      {children}
+    </PlaylistProvider>
+  );
+}
