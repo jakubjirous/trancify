8de5ef6290a90481385ca73e1567fac19cff3b5c
Jakub Jirous <info@jakubjirous.dev>
2024-10-14 10:38:18 -0700

    core: update docs for cn

--- src/utils/cn.ts
+++ src/utils/cn.ts
@@ -1,6 +1,10 @@
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
+/**
+ * Helper function for merging Tailwind classes
+ * @param inputs
+ */
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
