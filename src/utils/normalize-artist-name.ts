0efdb260320bd6fb23bcffe0500732d131649d3f
Jakub Jirous <info@jakubjirous.dev>
2024-10-14 10:40:08 -0700

    utils: add function for normalize artist names

---
+++ src/utils/normalize-artist-name.ts
@@ -0,0 +1,17 @@
+/**
+ * Normalize artist name
+ * @param name
+ */
+export default function normalizeArtistName(name: string): string[] {
+  const conjunctions = ["and", "&", "feat", "feat.", "with", "vs", "vs."];
+
+  const artistNames = name
+    .split(new RegExp(`\\s(${conjunctions.join("|")})\\s`))
+    .filter((artistName) => !conjunctions.includes(artistName.toLowerCase()));
+
+  return artistNames.map((artistName) => {
+    const sanitizedName = artistName.replace(/[^a-zA-Z0-9 ]/g, "");
+    const words = sanitizedName.split(/\s+/);
+    return words.join(" ");
+  });
+}
