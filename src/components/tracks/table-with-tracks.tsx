3e951e77072982d59bc6eca1ef53a0df82323860
Jakub Jirous <info@jakubjirous.dev>
2024-10-15 14:54:29 -0700

    ui: add actions column to the tracks table

--- src/components/tracks/table-with-tracks.tsx
+++ src/components/tracks/table-with-tracks.tsx
@@ -1,5 +1,5 @@
 import TrackRow from "@/components/tracks/track-row";
-import { ScrollArea } from "@/components/ui/scroll-area";
+import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
 import {
   Table,
   TableBody,
@@ -17,15 +17,19 @@ export default function TableWithTracks({
   headerStyles,
 }: { tracks: Track[]; headerStyles?: string }) {
   return (
-    <ScrollArea className="h-full">
-      <Table>
+    <ScrollArea className="w-full">
+      <ScrollBar orientation="horizontal" />
+      <Table className="w-full">
         <TableHeader className={cn(headerStyles)}>
           <TableRow className="border-foreground/10 border-b">
             <TableHead className="w-10 pl-8">#</TableHead>
             <TableHead>Title</TableHead>
             <TableHead>Album</TableHead>
             <TableHead className="w-10 pr-8 text-right">
-              <Clock className="ml-auto" size={16} />
+              <Clock className="ml-auto size-4" />
+            </TableHead>
+            <TableHead>
+              <span className="sr-only">Actions</span>
             </TableHead>
           </TableRow>
         </TableHeader>
