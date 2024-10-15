3336dc92bd15a710fd628a2b685d0d0dcbb25b75
Jakub Jirous <info@jakubjirous.dev>
2024-10-15 14:52:18 -0700

    lib: add sever actions and mutation for creating and deleting playlist

---
+++ src/lib/actions/index.ts
@@ -0,0 +1,5 @@
+import addToPlaylistAction from "./add-to-playlist-action";
+import createPlaylistAction from "./create-playlist-action";
+import deletePlaylistAction from "./delete-playlist-action";
+
+export { createPlaylistAction, addToPlaylistAction, deletePlaylistAction };
