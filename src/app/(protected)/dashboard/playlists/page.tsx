e9614b0bdfd78727cc8c692b1a64b5b4ff6a0046
Jakub Jirous <info@jakubjirous.dev>
2024-10-19 12:31:17 -0700

    ui: add action to the title component

--- src/app/(protected)/dashboard/playlists/page.tsx
+++ src/app/(protected)/dashboard/playlists/page.tsx
@@ -1,3 +1,4 @@
+import AddPlaylist from "@/components/playlist/add-playlist";
 import PlaylistCards from "@/components/playlist/playlist-cards";
 import Title from "@/components/title";
 import React from "react";
@@ -6,7 +7,7 @@ export default async function PlaylistsPage() {
   return (
     <div className="grid h-full grid-rows-1">
       <section>
-        <Title title="Your playlists" />
+        <Title title="Your playlists" action={<AddPlaylist />} />
         <PlaylistCards />
       </section>
     </div>
--- src/components/title.tsx
+++ src/components/title.tsx
@@ -1,13 +1,15 @@
 "use client";
 
-import React from "react";
+import React, { ReactNode } from "react";
 
-export default function Title({ title }: { title: string }) {
+export default function Title({
+  title,
+  action,
+}: { title: string; action: ReactNode }) {
   return (
-    <div className="flex h-[6rem] w-full bg-transparent pt-8 backdrop-blur">
-      <h2 className="relative px-7 font-semibold text-xl tracking-tight">
-        {title}
-      </h2>
+    <div className="flex h-[6rem] w-full items-start justify-between bg-transparent px-8 pt-8 backdrop-blur">
+      <h2 className="relative font-semibold text-xl tracking-tight">{title}</h2>
+      {action}
     </div>
   );
 }
