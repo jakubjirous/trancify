f5fc821814f5aa9bf012b687145c21a94ad50012
Jakub Jirous <info@jakubjirous.dev>
2024-10-19 19:07:17 -0700

    db: add search track query

--- src/lib/db/queries/index.ts
+++ src/lib/db/queries/index.ts
@@ -2,10 +2,12 @@ import getAllPlaylists from "./get-all-playlists";
 import getAllPlaylistsWithArtists from "./get-all-playlists-with-artists";
 import getAllTracks from "./get-all-tracks";
 import getPlaylistWithTracks from "./get-playlist-with-tracks";
+import searchTracks from "./search-tracks";
 
 export {
   getAllTracks,
   getAllPlaylists,
   getPlaylistWithTracks,
   getAllPlaylistsWithArtists,
+  searchTracks,
 };
