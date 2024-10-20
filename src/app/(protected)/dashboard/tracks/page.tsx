43bf51edabfdfc34c993d532611633f7779b4fe6
Jakub Jirous <info@jakubjirous.dev>
2024-10-19 19:09:17 -0700

    ui: integrate search tracks

--- src/app/(protected)/dashboard/tracks/page.tsx
+++ src/app/(protected)/dashboard/tracks/page.tsx
@@ -1,15 +1,21 @@
 import Title from "@/components/title";
 import TableWithTracks from "@/components/tracks/table-with-tracks";
-import { getAllTracks } from "@/lib/db/queries";
-import React from "react";
+import { getAllTracks, searchTracks } from "@/lib/db/queries";
+import React, { Suspense } from "react";
 
-export default async function TracksPage() {
-  const tracks = await getAllTracks();
+export default async function TracksPage({
+  searchParams,
+}: { searchParams: Promise<{ search: string }> }) {
+  const { search } = await searchParams;
+
+  const tracks = search ? await searchTracks(search) : await getAllTracks();
 
   return (
     <section className="grid h-full grid-rows-[auto,minmax(0,1fr)]">
       <Title title="Your tracks" />
-      <TableWithTracks tracks={tracks} />
+      <Suspense fallback="">
+        <TableWithTracks tracks={tracks} search={search} />
+      </Suspense>
     </section>
   );
 }
