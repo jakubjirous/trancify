a94bdc3ab96fef410398a59018d04a6ea661cc2c
Jakub Jirous <info@jakubjirous.dev>
2024-10-19 19:08:14 -0700

    fix: optional action

--- src/components/title.tsx
+++ src/components/title.tsx
@@ -5,7 +5,7 @@ import React, { ReactNode } from "react";
 export default function Title({
   title,
   action,
-}: { title: string; action: ReactNode }) {
+}: { title: string; action?: ReactNode }) {
   return (
     <div className="flex h-[6rem] w-full items-start justify-between bg-transparent px-8 pt-8 backdrop-blur">
       <h2 className="relative font-semibold text-xl tracking-tight">{title}</h2>
