14d1fd867d5ecf43795ee31cdd04be70fa98f6c8
Jakub Jirous <info@jakubjirous.dev>
2024-10-19 19:03:35 -0700

    ui: remove bg pattern

--- src/app/(protected)/dashboard/layout.tsx
+++ src/app/(protected)/dashboard/layout.tsx
@@ -1,21 +1,19 @@
 import NowPlaying from "@/components/now-playing/now-playing";
 import Player from "@/components/player/player";
-import { Sidebar } from "@/components/sidebar";
+import { Sidebar } from "@/components/sidebar/sidebar";
 import ProtectedProviders from "@/providers/protected-providers";
 import { ReactNode } from "react";
 
 export default function DashboardLayout({ children }: { children: ReactNode }) {
   return (
     <ProtectedProviders>
-      <div className="relative grid h-dvh grid-cols-[24rem,4fr] grid-rows-[1fr,8rem] overflow-clip text-foreground">
+      <div className="grid h-dvh grid-cols-[24rem,4fr] grid-rows-[1fr,8rem] overflow-clip text-foreground">
         <Sidebar />
         <main className="flex h-[calc(100dvh-8rem)] justify-between">
           <div className="w-full">{children}</div>
           <NowPlaying />
         </main>
         <Player />
-
-        <div className="-z-10 -rotate-12 absolute top-[-10vw] left-[-10vw] aspect-square w-[30vw] animate-spin-slow bg-gradient-to-br from-purple-900 to-background blur-[10vw] [--spin-duration:5s]"></div>
       </div>
     </ProtectedProviders>
   );
